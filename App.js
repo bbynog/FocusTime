8188d86e7ab9f64038b06a5d1e6e42da4f7b3673
Gabriel Nogueira <bbynogueira@gmail.com>
2021-06-07 09:40:43 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { Focus } from './src/features/focus/Focus';
+import { FocusHistory } from './src/features/focus/FocusHistory';
+
+import { Timer } from './src/features/timer/Timer';
+import { colors } from './src/utils/colors';
+import { spacing } from './src/utils/sizes';
+
+const STATUSES = {
+  CANCELLED: 0,
+  COMPLETED: 1,
+};
+
+export default function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  const addSubjectToHistoryWithStatus = (subject, status) => {
+    setFocusHistory([...focusHistory, { key: String(focusHistory.length + 1), subject, status }]);
+  };
+
+  const onClear = () => {
+    setFocusHistory([]);
+  };
+
+  const saveFocusHistory = async () => {
+    try {
+      await AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory));
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  const loadFocusHistory = async () => {
+    try {
+      const history = await AsyncStorage.getItem('focusHistory');
+      if (history && JSON.parse(history).length) {
+        setFocusHistory(JSON.parse(history));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(()=>{
+    loadFocusHistory();
+  }, [])
+
+  useEffect(()=>{
+    saveFocusHistory();
+  }, [focusHistory])
+
+  return (
+    <View style={styles.container}>
+      {focusSubject ? (
+        <Timer
+          focusSubject={focusSubject}
+          onTimerEnd={() => {
+            addSubjectToHistoryWithStatus(focusSubject, STATUSES.COMPLETED);
+            setFocusSubject(null);
+          }}
+          clearSubject={() => {
+            addSubjectToHistoryWithStatus(focusSubject, STATUSES.CANCELLED);
+            setFocusSubject(null);
+          }}
+        />
+      ) : (
+        <>
+          <Focus addSubject={setFocusSubject} />
+          <FocusHistory focusHistory={focusHistory} onClear={onClear} />
+        </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? spacing.md : spacing.lg,
+    backgroundColor: colors.darkBlue,
+  },
+});
